f43dd8ebaab880e3e05318b1e2ff19743977ce89
Fahrezy <fahrezyromeropurba@gmail.com>
2025-06-07 00:08:06 +0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from "./Navbar";
+import imghero from './imghero.png'
+import instagram from './instagram.png'
+import linkidn from './linkidn.png'
+import ezy from './ezy.pdf'
+
+export default function Hero() {
+    return (
+        <div className='relative overflow-hidden min-h-[550px] sm:min-h-[600px] flex flex-col items-center'>
+            <div className='md:h-[550px] h-[500px] w-[450px] bg-gradient-to-r absolute from-[#6d2897]
+            via-[#8e6cf5]to-[#bb61c5] transform rotate-45 z-0 right-2 top-28 rounded-3x1'
+            ></div>
+            <Navbar />
+        </div>
+    )
+}
